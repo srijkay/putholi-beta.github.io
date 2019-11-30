9c9a4d4ef949f59424d72675c115baefdbd82307
Kamalkanth <dkamalkanth@gmail.com>
2019-11-30 17:26:13 -0500

    Updates

---
+++ precache-manifest.2f4d39cd3af8e7573d10ce47dd9e60c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3156f101ca4261723dd5659fe28fc0fc",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "0d20e9976b45724ce5eb",
+    "url": "/react-admin/static/css/main.92640dc6.chunk.css"
+  },
+  {
+    "revision": "ad1a6325ae01274acf06",
+    "url": "/react-admin/static/js/2.876de864.chunk.js"
+  },
+  {
+    "revision": "0d20e9976b45724ce5eb",
+    "url": "/react-admin/static/js/main.18d618b5.chunk.js"
+  },
+  {
+    "revision": "4f989c5c59420d262ac3",
+    "url": "/react-admin/static/js/runtime~main.f8761412.js"
+  }
+]);
